7d4594233e737b794f4f97264146244aca3677b0
Marius <Marius.Supercode@gmail.com>
2022-12-06 14:04:04 +0100

    styling fehlt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import { useState } from 'react';
+import Filter from './components/Filter/Filter';
+import Header from './components/Header/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
+
   const [searchterm, setSearchTerm] = useState("?limit=100000&offset=0.");
   fetch(`https://pokeapi.co/api/v2/pokemon/${searchterm}`)
     .then(res => res.json())
     .then((res) => {
       console.log(res);
+
     });
 
   return (
     <div className="App">
-
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/:id" element={<Header />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,38 @@
+import { Link } from "react";
+import './Filter.css';
+function Filter({ }) {
+    // console.log({ move: "out" });
+
+    function filter(a) {
+        // console.log(a);
+    }
+    return (
+        <section>
+            <div>
+                <button onClick={filter("bug")}>Bug</button>
+                <button onClick={filter("dark")}>Dark</button>
+                <button onClick={filter("dragon")}>Dragon</button>
+                <button onClick={filter("electric")}>Electric</button>
+                <button onClick={filter("fairy")}>Fairy</button>
+                <button onClick={filter("fighting")}>Fighting</button>
+                <button onClick={filter("fire")}>Fire</button>
+                <button onClick={filter("flying")}>Flying</button>
+                <button onClick={filter("ghost")}>Ghost</button>
+                <button onClick={filter("grass")}>Grass</button>
+                <button onClick={filter("ground")}>Ground</button>
+                <button onClick={filter("ice")}>Ice</button>
+                <button onClick={filter("normal")}>Normal</button>
+                <button onClick={filter("plant")}>Plant</button>
+                <button onClick={filter("poison")}>poison</button>
+                <button onClick={filter("psychic")}>psychic</button>
+                <button onClick={filter("rock")}>rock</button>
+                <button onClick={filter("steel")}>steel</button>
+                <button onClick={filter("water")}>water</button>
+            </div>
+            <button type="Button">Search</button>
+        </section>
+    );
+}
+
+
+export default Filter;
