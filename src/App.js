a64546e520e1a1d59f4a793478d8096693d294cd
Marius <Marius.Supercode@gmail.com>
2022-12-06 14:20:00 +0100

    changes app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import './App.css';
 import { useState } from 'react';
-import Filter from './components/Filter/Filter';
-import Header from './components/Header/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import List from './pages/ListPage/ListPage';
 
 function App() {
-
   const [searchterm, setSearchTerm] = useState("?limit=100000&offset=0.");
   fetch(`https://pokeapi.co/api/v2/pokemon/${searchterm}`)
     .then(res => res.json())
     .then((res) => {
       console.log(res);
-
     });
 
   return (
     <div className="App">
-      <Header />
-      <Router>
-        <Routes>
-          <Route path="/:id" element={<Header />} />
-        </Routes>
-      </Router>
+      <List />
     </div>
   );
 }
