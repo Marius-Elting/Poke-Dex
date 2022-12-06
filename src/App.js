3e9e9f23f0c6f2313ad1e0d18cf991835deb9b55
Marius <Marius.Supercode@gmail.com>
2022-12-06 10:43:13 +0100

    base structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [searchterm, setSearchTerm] = useState("?limit=100000&offset=0.");
+  fetch(`https://pokeapi.co/api/v2/pokemon/${searchterm}`)
+    .then(res => res.json())
+    .then((res) => {
+      console.log(res);
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
